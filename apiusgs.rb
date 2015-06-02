74fd3dcd2c3ee5288a4fd23a4b96b54de4c5beab
Isaac Godfried <igodfried@isaac26.com>
2015-06-01 23:28:07 -0400

    nokogir

--- apiusgs.rb
+++ apiusgs.rb
@@ -1,2 +1,9 @@
-Class USGSA
-end 
+require 'nokogiri'
+require 'open-uri'
+	xml_file = open('http://waterservices.usgs.gov/nwis/iv/?format=waterml,1.1&sites=01037000&parameterCd=00060,00065')
+ 	doc = Nokogiri::XML(xml_file)
+ 	doc.xpath('//ns1:values/ns1:value').each do |node|
+ 	puts node.text
+ 	end 
+
+	
